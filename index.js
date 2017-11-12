8eeb2d13f5fc385aedaab5f32f9593f13f51eb65
marls628 <marlyshawley@outlook.com>
2017-11-12 19:46:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
    if (distance <= 400) {
      return 'This one is on me!'
    }
-   if (distance > 2000) {
+   else if (distance > 2000 && distance < 2500) {
      return 'I will gladly take your thirty bucks.'
    }
    if (distance > 2500) {
