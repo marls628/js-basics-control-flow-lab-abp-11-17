6acc58d1d22f87b52b6523a21e9b8301d49accd4
marls628 <marlyshawley@outlook.com>
2017-11-12 21:23:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,10 @@ function ternaryCheckCity(city) {
     return 'No go.'
   }
 }
-
+function switchOnCharmFromTip(tipString) {
+  switch (tipString) {
+  case 'generous':
+  return ('Thank you so much')
+  break;
+  }
+}
