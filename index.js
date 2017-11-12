f502cb2d7fc96cacce311bc3f7841128ce6699eb
marls628 <marlyshawley@outlook.com>
2017-11-12 20:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,18 +3,4 @@
    if (distance <= 400) {
      return 'This one is on me!'
    }
-   else if (distance > 2000 && distance < 2500) {
-     return 'I will gladly take your thirty bucks.'
-   }
-   if (distance > 2500) {
-     return 'No can do.'
-   }
- }
-function ternaryCheckCity(city) {
-  if (city === "NYC") {
-    return "Ok, sounds good."
-  } else if {
-    return "No go"
-  }
-
-}
+  
