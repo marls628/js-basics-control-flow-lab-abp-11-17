d6c49ea8f4403e7d024a5209201d799c510d7827
marls628 <marlyshawley@outlook.com>
2017-11-12 20:28:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function scuberGreetingForFeet(distance) {
   if (distance < 400) {
     return 'This one is on me!'
   }
+  if ((distance > 200) && (distance < 2500)){
+    return 'I will gladly take your thirty bucks.'
+  }
 }
