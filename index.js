358a3f85e9994992d52cb486340c9dce2441304f
marls628 <marlyshawley@outlook.com>
2017-11-12 20:30:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scuberGreetingForFeet(distance) {
     else if ((distance > 2000) && (distance < 2500)){
     return 'I will gladly take your thirty bucks.'
   }
-  if (distance > 2500) {
+    else if (distance > 2500) {
     return 'No can do'
   }
 }
