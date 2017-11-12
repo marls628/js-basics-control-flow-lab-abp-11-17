4cc395e0d0f095333b415ded49d41e32775a1d74
marls628 <marlyshawley@outlook.com>
2017-11-12 20:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ function scuberGreetingForFeet(distance) {
 function ternaryCheckCity(city) {
   if (city === 'NYC') {
     return 'Ok, sounds good.'
-  }
+  }  else {
+    return 'No go.'
+  } 
 }
